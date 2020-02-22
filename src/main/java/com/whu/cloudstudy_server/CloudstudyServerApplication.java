19e306f30de5ca675d69834d7b3a885ddef9a202
1301702486 <1301702486@qq.com>
2020-02-22 16:37:26 +0800

    初始空项目

---
+++ src/main/java/com/whu/cloudstudy_server/CloudstudyServerApplication.java
@@ -0,0 +1,13 @@
+package com.whu.cloudstudy_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudstudyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudstudyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whu/cloudstudy_server/CloudstudyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whu.cloudstudy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudstudyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
