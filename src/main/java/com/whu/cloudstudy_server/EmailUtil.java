c90aa27c3465f3a389640f705efd5c8b71a17386
1301702486 <1301702486@qq.com>
2020-02-25 18:29:31 +0800

    验证码相关

---
+++ src/main/java/com/whu/cloudstudy_server/EmailUtil.java
@@ -0,0 +1,59 @@
+package com.whu.cloudstudy_server;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * @author 郭瑞景
+ * @date 2020-02-24
+ */
+public class EmailUtil {
+    private String from = "whucloudstudy@yeah.net";
+
+    private String password = "2020yunzixi";
+
+    private String host = "smtp.yeah.net";
+
+    private Properties properties = System.getProperties();
+
+    /**
+     * 发送邮件
+     *
+     * @param to
+     * @param subject
+     * @param content
+     */
+    public boolean sendMessage(String to, String subject, String content) {
+        properties.setProperty("mail.smtp.host", host);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.transport.protocol", "SMTP");
+        properties.put("mail.debug", "true");
+        properties.setProperty("mail.smtp.socketFactory.port", "465");
+
+        Session session = Session.getDefaultInstance(properties, new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        MimeMessage message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject);
+            message.setContent(content, "text/html;charset=UTF-8");
+            Transport.send(message);
+            System.out.println("邮件发送成功！");
+            System.out.println("详情: To " + to + " " + message.getSubject());
+            return true;
+        } catch (MessagingException e) {
+            System.out.println("邮件发送失败！");
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
--- src/main/java/com/whu/cloudstudy_server/controller/ValidateCodeController.java
+++ src/main/java/com/whu/cloudstudy_server/controller/ValidateCodeController.java
@@ -1,8 +1,81 @@
 package com.whu.cloudstudy_server.controller;
 
+import com.whu.cloudstudy_server.Response;
+import com.whu.cloudstudy_server.service.ValidateCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author 郭瑞景
  * @date 2020-02-24
  */
+
+@RestController
+@RequestMapping(value = "/user")
 public class ValidateCodeController {
+    @Autowired
+    private ValidateCodeService codeService;
+
+    @PostMapping(value = "/sendValidateCode")
+    public Response sendValidateCode(String email, Integer type) {
+        int ret = codeService.sendCode(email, type);
+        Response response;
+        switch (ret) {
+            case 0:
+                response = new Response(0,"成功",null);
+                break;
+            case -1:
+                response = new Response(-1,"邮箱重复",null);
+                break;
+            case -2:
+                response = new Response(-2,"用户不存在",null);
+                break;
+            case -3:
+                response = new Response(-3,"验证码发送失败",null);
+                break;
+            default:
+                response = new Response(-4,"失败",null);
+                break;
+        }
+        return response;
+    }
+
+    @PostMapping(value = "/register")
+    public Response register(String name, String password, Character gender,
+                             String introduction, String email, Integer code, Integer age) {
+        int ret = codeService.register(name, password, gender, introduction, email, code, age);
+        Response response;
+        switch (ret) {
+            case 0:
+                response = new Response(0,"成功",null);
+                break;
+            case -1:
+                response = new Response(-1,"验证码错误",null);
+                break;
+            default:
+                response = new Response(-2,"失败",null);
+                break;
+        }
+        return response;
+    }
+
+    @PostMapping(value = "/validateCode")
+    public Response validateCode(String email, Integer code) {
+        int ret = codeService.validateCode(email, code);
+        Response response;
+        switch (ret) {
+            case 0:
+                response = new Response(0,"成功",null);
+                break;
+            case -1:
+                response = new Response(-1,"验证码错误",null);
+                break;
+            default:
+                response = new Response(-2,"失败",null);
+                break;
+        }
+        return response;
+    }
 }
