0e21ae020105dce8fc861aa5f6f273038dfcaddd
Rea1One <2697732841@qq.com>
2020-03-01 10:04:18 +0800

    UserAndPlanet实体类接口和映射

--- src/main/java/com/whu/cloudstudy_server/entity/UserAndPlanet.java
+++ src/main/java/com/whu/cloudstudy_server/entity/UserAndPlanet.java
@@ -45,3 +45,4 @@ public class UserAndPlanet {
         this.enterTime=enterTime;
     }
 }
+
---
+++ src/main/java/com/whu/cloudstudy_server/mapper/UserAndPlanetMapper.java
@@ -0,0 +1,16 @@
+package com.whu.cloudstudy_server.mapper;
+
+//import com.whu.cloudstudy_server.entity.Planet;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author 叶瑞雯
+ * @date 2020-03-01
+ */
+
+@Mapper
+public interface UserAndPlanetMapper {
+
+}
