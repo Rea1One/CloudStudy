80f5c6c96663f31e7989458840668bccfb484ca5
1301702486 <1301702486@qq.com>
2020-03-02 12:15:22 +0800

    Planet映射补充

--- src/main/java/com/whu/cloudstudy_server/mapper/PlanetMapper.java
+++ src/main/java/com/whu/cloudstudy_server/mapper/PlanetMapper.java
@@ -2,6 +2,8 @@ package com.whu.cloudstudy_server.mapper;
 
 import com.whu.cloudstudy_server.entity.Planet;
 
+import java.util.List;
+
 /**
  * @author 郭瑞景
  * @date 2020-03-02
@@ -9,6 +11,7 @@ import com.whu.cloudstudy_server.entity.Planet;
 public interface PlanetMapper {
     Planet findPlanetById(Integer id);
     Planet findPlanetByCode(String code);
+    List<Planet> findPlanetByName(String name);
     void insertPlanet(Planet planet);
     void updatePlanetInfo(Planet planet);
     void deletePlanet(Integer id);
