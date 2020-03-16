5b98a707f7fb21bdca30ae88851d246f80fbb412
Rea1One <2697732841@qq.com>
2020-03-16 16:27:58 +0800

    学习统计1接口

--- src/main/java/com/whu/cloudstudy_server/service/StudyRecordService.java
+++ src/main/java/com/whu/cloudstudy_server/service/StudyRecordService.java
@@ -2,9 +2,7 @@ package com.whu.cloudstudy_server.service;
 
 import com.whu.cloudstudy_server.entity.Planet;
 import com.whu.cloudstudy_server.entity.StudyRecord;
-import com.whu.cloudstudy_server.entity.User;
 import com.whu.cloudstudy_server.mapper.StudyRecordMapper;
-import com.whu.cloudstudy_server.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
