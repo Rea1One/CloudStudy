40e60a9d68d2459294c27419c4b32d614638d08d
ChrisLongChen <52145331+ChrisLongChen@users.noreply.github.com>
2020-03-28 13:44:41 +0800

    找回密码

---
+++ src/main/java/com/whu/cloudstudy_server/service/FindCodeService.java
@@ -0,0 +1,41 @@
+package com.whu.cloudstudy_server.service;
+
+import com.whu.cloudstudy_server.util.Response;
+import com.whu.cloudstudy_server.entity.*;
+import com.whu.cloudstudy_server.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Author: 胡龙晨
+ * Date: 2020-02-27
+ */
+
+@Service
+public class FindCodeService{
+    @Autowired(required = false)
+    private UserMapper userMapper;
+
+    /**
+     * 找回密码
+     *
+     * @param email
+     * @param newPassword
+     * @return
+     */
+    public int findPassword(String email,String newPassword){
+        User user=userMapper.findUserByEmail(email);
+        user.setPassword(newPassword);
+        int cnt = userMapper.updateUserInfo(user);
+        if (cnt > 0) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+}
