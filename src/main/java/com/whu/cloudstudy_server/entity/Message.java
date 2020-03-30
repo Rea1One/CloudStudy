adb6a795032e9f8aa5c030ac35ec806c51d5fe01
ChrisLongChen <52145331+ChrisLongChen@users.noreply.github.com>
2020-03-30 17:18:53 +0800

    留言实体类更新

---
+++ src/main/java/com/whu/cloudstudy_server/entity/Message.java
@@ -0,0 +1,66 @@
+package com.whu.cloudstudy_server.entity;
+
+import java.sql.Timestamp;
+
+/**
+ * @author 胡龙晨
+ * @date 2020-02-26
+ */
+public class Message{
+    private Integer id;
+    private Integer senderId;
+    private Integer receiverId;
+    private String content;
+    private Timestamp createTime;
+    private String photo;
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public Integer getSenderId(){
+        return senderId;
+    }
+
+    public void setSenderId(Integer senderId){
+        this.senderId = senderId;
+    }
+
+    public Integer getReceiverId(){
+        return receiverId;
+    }
+
+    public void setReceiverId(Integer receiverId){
+        this.receiverId = receiverId;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public Timestamp getCreateTime(){
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime){
+        this.createTime = createTime;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo=photo;
+    }
+
+
+}
