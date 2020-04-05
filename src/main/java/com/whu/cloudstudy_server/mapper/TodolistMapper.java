fc77b9dfd22df62af3e76f64cd348c0993c3e1f2
Rea1One <2697732841@qq.com>
2020-04-05 10:23:39 +0800

    学习统计接口修改

--- src/main/java/com/whu/cloudstudy_server/mapper/TodolistMapper.java
+++ src/main/java/com/whu/cloudstudy_server/mapper/TodolistMapper.java
@@ -2,6 +2,8 @@ package com.whu.cloudstudy_server.mapper;
 
 import com.whu.cloudstudy_server.entity.Todolist;
 
+import java.util.List;
+
 /**
  * @author 叶瑞雯
  * @date 2020-04-05
@@ -10,6 +12,6 @@ import com.whu.cloudstudy_server.entity.Todolist;
 public interface TodolistMapper {
     int insertTodolist(Todolist todolist);
     int deleteTodolistById(Integer id);
-    Todolist findTodolistByUserId(Integer userId);
+    List<Todolist> findTodolistByUserId(Integer userId);
 }
 
